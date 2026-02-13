18f7dab958f2e257de6a33d15fb2bb55c6f3bcbc
Bright Munemu <brightmunemu@gmail.com>
2026-02-13 05:10:08 +0100

    add

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(['INTERN', 'DESIGNER', 'ENGINEER', 'ADMIN'], {
+    message: 'Valid role is requested',
+  })
   role: 'INTERN' | 'DESIGNER' | 'ENGINEER' | 'ADMIN';
 }
